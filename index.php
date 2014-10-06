3a48810ff56a32a3d0ac95d31a259a9870da7430
Markus <markus.joemets@khk.ee>
2014-10-06 10:27:50 +0300

    algus tehtud

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;
+charset=utf-8">
+<title> harjutus-7 </title>
+</head>
+<body >
+
+ <h1> Haiiii </h1>
+
+ </body>
+
+ </html>
